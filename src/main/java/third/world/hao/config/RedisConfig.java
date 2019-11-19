c359e799b446d20313eabf317ec2eb27d323757e
maybenot <594180536@qq.com>
2019-11-19 22:28:01 +0800

    redis集成(half)

---
+++ src/main/java/third/world/hao/config/RedisConfig.java
@@ -0,0 +1,11 @@
+package third.world.hao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+
+}
---
+++ src/main/java/third/world/hao/controller/Redis.java
@@ -0,0 +1,7 @@
+package third.world.hao.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Redis {
+}
